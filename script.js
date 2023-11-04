f650974765fc5804b185a94e164674793df905c8
Loinbif <124847066+SrLoinbif@users.noreply.github.com>
2023-11-04 17:20:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,360 @@
+const questions = [
+  {
+      question: "Barang yang diakadkan disebut...",
+      optionA: "Sighat",
+      optionB: "Ma’qud ‘Alaih",
+      optionC: "Aqidain",
+      optionD: "Khulu",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Hak pelaku transaksi untuk memilih melanjutkan atau membatalkan transaksi disebut...",
+      optionA: "Muzara’ah",
+      optionB: "Syirkah",
+      optionC: "Salam",
+      optionD: "Khiyar",
+      correctOption: "optionD"
+  },
+
+  {
+      question: "Jenis syirkah yang ciri-cirinya adalah adanya syarat kesamaan modal, kerja, tanggung jawab, beban hutang, dan kesamaan laba adalah...",
+      optionA: "Syirkah Wujuh",
+      optionB: "Syirkah Muwafadah",
+      optionC: "Syirkah Amla’",
+      optionD: "Syirkah ‘Abdan",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Koperasi dan firma termasuk jenis syirkah...",
+      optionA: "Syirkah Amla’",
+      optionB: "Syirkah ‘Abdan",
+      optionC: "Syirkah Muwafadah",
+      optionD: "Syirkah Wujuh",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Berikut ini yang bukan merupakan rukun jual beli adalah...",
+      optionA: "Aqidain",
+      optionB: "Ma’qud ‘alaih",
+      optionC: "Sigah",
+      optionD: "Ihtikar",
+      correctOption: "optionD"
+  },
+
+  {
+      question: "Yang bukan syarat benda disebut cacat, yaitu…",
+      optionA: "Cacat pada bagian yang penting",
+      optionB: "Cacat barang tidak mengurangi manfaat",
+      optionC: "Cacat barang terjadi ketika barang masih di tangan penjual",
+      optionD: "Cacat yang ada sulit dihilangkan",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Bagaimana hukum bagi penjual untuk menjual barang yang cacat tanpa memberitahukan kepada pembeli?",
+      optionA: "Boleh",
+      optionB: "Mubah",
+      optionC: "Sunnah",
+      optionD: "Haram",
+      correctOption: "optionD"
+  },
+
+  {
+      question: "الْبَيِّعَانِ بِالْخِيَارِ مَا لَمْ يَتَفَرَّقَا (رواه البخاري) Hadis di atas merujuk pada salah satu jenis khiyar, yaitu...",
+      optionA: "Khiyar ru’yah",
+      optionB: "Khiyar majelis",
+      optionC: "Khiyar syarat",
+      optionD: "Khiyar ‘aibi",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "وَأَحَلَّ اللَّهُ الْبَيْعَ وَحَرَّمَ الرِّبَا Ayat tersebut membahas tentang...",
+      optionA: "Khiyar",
+      optionB: "Syirkah",
+      optionC: "Aqad",
+      optionD: "Jual beli",
+      correctOption: "optionD"
+  },
+
+  {
+      question: `Ciri-ciri dari muzara’ah  adalah sebagai berikut, kecuali...`,
+      optionA: "Penggarap merawat tanaman",
+      optionB: "Pemilik kebun memberikan lahan pertaniannya",
+      optionC: "Hasil panen hanya diberikan kepada penggarap",
+      optionD: "Benih dari penggarap",
+      correctOption: "optionC"
+  },
+
+  {
+      question: "Berikut yang tidak termasuk khiyar majlis yang sah adalah...",
+      optionA: "Tidak terjadi pada akad yang objek akadnya berupa barang",
+      optionB: "Tidak terjadi pada akad yang kepemilikannya bersifat otoritatif",
+      optionC: "Tidak terjadi pada akad yang bersifat rukhsah dari syariat",
+      optionD: "Tidak terjadi pada akad seperti ijarah",
+      correctOption: "optionA"
+  },
+
+  {
+      question: "Najla ingin membeli baju melalui e commerce. Dia membaca deskripsi produk yang akan dibeli, kemudian Najla melakukan akad secara online dengan penjual. 2 hari kemudian, barang yang dipesan Najla sampai di rumahnya. Barang yang sampai di rumah Najla, sama persis dengan deskripsi barang yang telah ia pesan. Hukum dan jenis transaksi jual beli yang dilakukan Najla adalah",
+      optionA: "Haram, Bai Ghoib",
+      optionB: "Mubah, Bai Mausuf Fi Zimmah",
+      optionC: "Makruh, Bai Musyahadah",
+      optionD: "Mubah, Bai Musyahadah",
+      correctOption: "optionB"
+  },
+
+
+  {
+      question: "Perusahaan PT. Gavin Racing  dengan Perusahaan PT. Kimia Ciputat sama-sama memiliki reputasi baik terhadap sesamanya.  oleh karena itu, ia melakukan kerjasama antar perusahaannya yang keuntungannya akan dibagi setengahnya. contoh diatas merupakan contoh syirkah...",
+      optionA: "Syirkah Abdan",
+      optionB: "Syirkah Wujuh",
+      optionC: "Syirkah Amla’",
+      optionD: "Syirkah Muwafadah",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "مَنِ اشْتَرَى مَالَمَ يَرَهُ فَلَهُ الْخِيَارُ إِذَا رَأَهُ. (رواه الترمذي) Sarah membeli tempat pensil melalui e-commerce. Sarah bersepakat dengan pembeli bahwa ia membeli dua tempat pensil berwarna biru dan merah, dengan harga yang juga telah disepakati. Namun, ketika barang sudah sampai, ternyata tempat pensil yang datang berwarna kuning dan hijau, dengan bentuk yang berbeda dari kesepakatan. Mengetahui hal itu, Sarah mengembalikan barang tersebut dan meminta untuk ditukar dengan barang yang sesuai dengan kesepakatan. Jika dikaitkan dengan hadis di atas, maka hukum dan jenis khiyar yang dilakukan Sarah untuk meminta tukar tempat pensilnya adalah...",
+      optionA: "Boleh, Khiyar aib",
+      optionB: "Haram, Khiyar majelis",
+      optionC: "Makruh, Khiyar syarat",
+      optionD: "Boleh, Khiyar syarat",
+      correctOption: "optionA"
+  },
+
+  {
+      question: "Razan membeli sebuah peralatan laboratorium kimia untuk melakukan penelitiannya dari perusahaan PT. Kimia Majida. Razan dapat melihat barang-barang yang perusahaan tersebut jual. Namun, ketika Razan ingin membeli barang yang ia inginkan, ternyata perusahaan tersebut kehabisan barang tersebut (kehabisan barang yang dijual, yang untuk dilihat  masih ada). Tetapi, barang tersebut sudah ada kembali 5 hari setelahnya, oleh karena itu Razan dan perusahaan tersebut sepakat untuk menunggu pada hari tersebut, namun Razan memutuskan membayar pada hari ini. Jual Beli yang dilakukan Razan dengan perusahaan tersebut termasuk pada Jual Beli",
+      optionA: "Jual Beli Muwafadah",
+      optionB: "Jual Beli Khulu’",
+      optionC: "Jual Beli Salam",
+      optionD: "Jual Beli Ghaib",
+      correctOption: "optionC"
+  },
+
+  {
+      question: "Syirkah Amla disebut juga syirkah kerja",
+      optionA: "benar",
+      optionB: "salah",
+      optionC: "afaan tuch",
+      optionD: "Aq Lupa hehe",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Salah satu rukun jual beli adalah sighat",
+      optionA: "benar",
+      optionB: "salah",
+      optionC: "afaan tuch",
+      optionD: "Aq Lupa hehe",
+      correctOption: "optionA"
+  },
+
+  {
+      question: "Muzaro’ah dan musaqah dibedakan berdasarkan persentase bagi hasil antara pemilik lahan dan penggarap",
+      optionA: "benar",
+      optionB: "salah",
+      optionC: "afaan tuch",
+      optionD: "Aq Lupa hehe",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Firma, CV, dan koperasi termasuk contoh dari Syirkah Wujuh",
+      optionA: "benar",
+      optionB: "salah",
+      optionC: "afaan tuch",
+      optionD: "Aq Lupa hehe",
+      correctOption: "optionB"
+  },
+
+  {
+      question: "Penggarap hanya merawat tanaman hingga panen merupakan ciri dari musaqah",
+      optionA: "benar",
+      optionB: "salah",
+      optionC: "afaan tuch",
+      optionD: "Aq Lupa hehe",
+      correctOption: "optionA"
+  }
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+  //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+  while (shuffledQuestions.length <= 9) {
+      const random = questions[Math.floor(Math.random() * questions.length)]
+      if (!shuffledQuestions.includes(random)) {
+          shuffledQuestions.push(random)
+      }
+  }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+  handleQuestions()
+  const currentQuestion = shuffledQuestions[index]
+  document.getElementById("question-number").innerHTML = questionNumber
+  document.getElementById("player-score").innerHTML = playerScore
+  document.getElementById("display-question").innerHTML = currentQuestion.question;
+  document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+  document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+  document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+  document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+  const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+  const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+  const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+  let correctOption = null
+
+  options.forEach((option) => {
+      if (option.value === currentQuestionAnswer) {
+          //get's correct's radio input with correct answer
+          correctOption = option.labels[0].id
+      }
+  })
+
+  //checking to make sure a radio input has been checked or an option being chosen
+  if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+      document.getElementById('option-modal').style.display = "flex"
+  }
+
+  //checking if checked radio button is same as answer
+  options.forEach((option) => {
+      if (option.checked === true && option.value === currentQuestionAnswer) {
+          document.getElementById(correctOption).style.backgroundColor = "green"
+          playerScore++ //adding to player's score
+          indexNumber++ //adding 1 to index so has to display next question..
+          //set to delay question number till when next question loads
+          setTimeout(() => {
+              questionNumber++
+          }, 1000)
+      }
+
+      else if (option.checked && option.value !== currentQuestionAnswer) {
+          const wrongLabelId = option.labels[0].id
+          document.getElementById(wrongLabelId).style.backgroundColor = "red"
+          document.getElementById(correctOption).style.backgroundColor = "green"
+          wrongAttempt++ //adds 1 to wrong attempts 
+          indexNumber++
+          //set to delay question number till when next question loads
+          setTimeout(() => {
+              questionNumber++
+          }, 1000)
+      }
+  })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+  checkForAnswer() //check if player picked right or wrong option
+  unCheckRadioButtons()
+  //delays next question displaying for a second just for some effects so questions don't rush in on player
+  setTimeout(() => {
+      if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+          NextQuestion(indexNumber)
+      }
+      else {
+          handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+      }
+      resetOptionBackground()
+  }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+  const options = document.getElementsByName("option");
+  options.forEach((option) => {
+      document.getElementById(option.labels[0].id).style.backgroundColor = ""
+  })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+  const options = document.getElementsByName("option");
+  for (let i = 0; i < options.length; i++) {
+      options[i].checked = false;
+  }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+  let remark = null
+  let remarkColor = null
+  let remarkImg = null
+
+  // condition check for player remark and remark color
+  if (playerScore <= 3) {
+      remark = "Astaghfirullah Brother. You should probably learn more about akad and kepemilikan"
+      remarkColor = "red"
+      remarkImg = "Astaghfirullah.gif"
+  }
+  else if (playerScore >= 4 && playerScore < 7) {
+      remark = "It's ok brother, You tried your best and that's all that matters."
+      remarkColor = "orange"
+      remarkImg = "subhanallah.jpeg"
+  }
+  else if (playerScore >= 7) {
+      remark = "Alhamdulillah Brother. You got a very good score mashaallah. please teach us sepuh!"
+      remarkColor = "green"
+      remarkImg = "Alhamdulillah.jph"
+  }
+  const playerGrade = (playerScore / 10) * 100
+
+  //data to display to score board
+  document.getElementById('remarks').innerHTML = remark
+  document.getElementById('remarks').style.color = remarkColor
+  document.getElementById('remarksImg').innerHTML = '<img src="' + remarkImg + '" width="110" height="110">'
+  document.getElementById('grade-percentage').innerHTML = playerGrade
+  document.getElementById('wrong-answers').innerHTML = wrongAttempt
+  document.getElementById('right-answers').innerHTML = playerScore
+  document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+  questionNumber = 1
+  playerScore = 0
+  wrongAttempt = 0
+  indexNumber = 0
+  shuffledQuestions = []
+  NextQuestion(indexNumber)
+  document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+  document.getElementById('option-modal').style.display = "none"
+}
+
+function credit() {
+    location.href = "credit.html";
+  }
+
+function materi() {
+    location.href = "materi.html";
+}
+
+function kuis() {
+    location.href = "Quiz.html";
+  }
